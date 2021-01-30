d253f7548b8e084e2eb865e7432d08a405659b2c
ss <ss@DESKTOP-8PQ5ARJ>
2021-01-30 12:25:45 +0530

    fIRST Commit

---
+++ Jav/src/com/selenium/learnings/JavaFirstProgram.java
@@ -0,0 +1,13 @@
+package com.selenium.learnings;
+
+public class JavaFirstProgram {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("First Program ");
+		System.out.println("Second Program");
+
+	}
+
+}
